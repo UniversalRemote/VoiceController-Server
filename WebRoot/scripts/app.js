19b7ff73fb31e40857ee614cbe479fdf0c69d568
Alejandro Chavez <alejandro_chavez@ultimatesoftware.com>
2017-11-15 10:55:07 -0500

    Changing console url

--- WebRoot/scripts/app.js
+++ WebRoot/scripts/app.js
@@ -3,14 +3,12 @@ var app = angular.module("VoiceController", ["ngRoute"]);
 
 app.config(function($routeProvider){
     $routeProvider
-        .when("/", {
+        .when("/console", {
             templateUrl: "views/console.html",
             controller: "Consoled"
         });
 });
 
 app.config(function($sceProvider) {
-    // Completely disable SCE.  For demonstration purposes only!
-    // Do not use in new projects or libraries.
     $sceProvider.enabled(false);
 });
